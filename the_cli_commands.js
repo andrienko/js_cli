501d502abc0a2cb1d53dccd67429a8137dcc404a
andrienko <andrienko@my.com>
2014-03-21 11:56:38 +0200

    Hidden commands

--- the_cli_commands.js
+++ the_cli_commands.js
@@ -1,44 +1,48 @@
-TheCLI.extend('help',function(data,cli){
+var the_cli = TheCLI;
+
+
+the_cli.extend('help',function(data,cli){
+    var hiddenCommands = ['cls','motd','barn','test','reset','command_with_error'];
     cli.nl().write('Following commands are available:').nl();
     for(var command in cli.commands){
-        cli.write('  - '+command);
+        if(hiddenCommands.indexOf(command)==-1) cli.write('  - '+command);
     }
     cli.nl();
 });
 
-TheCLI.extend('barn',function(data,cli){
+the_cli.extend('barn',function(data,cli){
     cli.clear();
     cli.write('\n<span style="color:#0f0">           x\n.-. _______|\n|=|/     /  \\\n| |_____|_""_|\n|_|_[X]_|____|\n</span>');
 });
 
-TheCLI.extend('test',function(data,cli){
+the_cli.extend('test',function(data,cli){
     cli.nl().write(JSON.stringify(data)).nl();
     console.log(data);
 });
 
-TheCLI.extend('echo',function(data,cli){
+the_cli.extend('echo',function(data,cli){
     cli.write(data.parametersText);
 });
 
-TheCLI.extend('eval',function(data,cli){
+the_cli.extend('eval',function(data,cli){
     cli.nl().write('evaluating ' + data.parametersText).nl();
     eval(data.parametersText);
 });
 
-TheCLI.extend('reset',function(data,cli){
+the_cli.extend('reset',function(data,cli){
     cli.clear();
     cli.init(cli.parent.id);
 });
 
-TheCLI.extend('command_with_error',function(){
+the_cli.extend('command_with_error',function(){
     the_undefined_function();
 });
 
-TheCLI.extend('line',function(data,cli){
+the_cli.extend('line',function(data,cli){
     cli.write('-'.repeat(cli.calculateDim()));
 })
 
-TheCLI.extend('centered',function(data,cli){
+the_cli.extend('centered',function(data,cli){
     var dim = cli.calculateDim();
     var text = data.parametersText;
     if(text=='')return;
@@ -48,4 +52,3 @@ TheCLI.extend('centered',function(data,cli){
     }
     else cli.write(' '.repeat((dim - text.length)/2)+text);
 })
-
